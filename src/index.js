19b636a046a456612b5cc2f4af7372949ffd6551
Gabriel <gabriel.liradefarias2@gmail.com>
2022-07-16 01:57:10 -0300

    finished fundo mobile

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import ReactDom from "react-dom";
 
 import Navbar from "./Componentes/Navbar";
 import Corpo from "./Componentes/Corpo";
+import FundoMobile from "./Componentes/FundoMobile";
 
 function App(){
     return(
         <div>
             <Navbar />
             <Corpo />
+            <FundoMobile />
         </div>
     )
 }
